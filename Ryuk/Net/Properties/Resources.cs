e204df306d6d4f275a3b41e0be66111e69579727
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2024-01-07 10:20:36 +0100

    Create Resources.cs

---
+++ Ryuk/Net/Properties/Resources.cs
@@ -0,0 +1,49 @@
+namespace Ryuk.Net.Properties
+{
+    using System;
+    using System.CodeDom.Compiler;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Resources;
+    using System.Runtime.CompilerServices;
+
+    [DebuggerNonUserCode, GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0"), CompilerGenerated]
+    internal class Resources
+    {
+        private static System.Resources.ResourceManager resourceMan;
+        private static CultureInfo resourceCulture;
+
+        internal Resources()
+        {
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (ReferenceEquals(resourceMan, null))
+                {
+                    resourceMan = new System.Resources.ResourceManager("Ryuk.Net.Properties.Resources", typeof(Resources).Assembly);
+                }
+                return resourceMan;
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture
+        {
+            get => 
+                resourceCulture;
+            set => 
+                resourceCulture = value;
+        }
+
+        internal static byte[] decrypter =>
+            (byte[]) ResourceManager.GetObject("decrypter", resourceCulture);
+
+        internal static string Source =>
+            ResourceManager.GetString("Source", resourceCulture);
+    }
+}
