6dbb258fb6f541fbb54b2024be745cf25e3fd671
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2024-01-07 10:34:11 +0100

    Update Program.cs

--- CustomWindowsForm/Program.cs
+++ CustomWindowsForm/Program.cs
@@ -8,9 +8,17 @@ namespace CustomWindowsForm
         [STAThread]
         private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BlackForm());
+    	    DialogResult dr = MessageBox.Show("WARNING! READ THIS MESSAGE FIRST", "You are running the Yashma/Chaos v5 Ransomware builder. Do you want to continue? By clicking on yes, you accept that you are liable and you will be legally responsible for any outcomes of this malware!", MessageBoxButtons.YesNo);
+    	    switch(dr)
+    	    {
+                case DialogResult.Yes:
+    		        Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new BlackForm());
+            	break;
+       	      case DialogResult.No:
+          		break;
+    	    }
         }
     }
 }
